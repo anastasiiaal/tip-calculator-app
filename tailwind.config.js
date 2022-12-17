b44fdcec048037f70a2da6f5026ba76ff31f8a54
Anastasiia <anastasia.alexeenko@gmail.com>
2022-12-17 19:02:38 +0100

    💥tailwind css HERE WE GO

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      'bg': '#C5E4E7',
+      'active': '#9FE8DF',
+      'primary': '#26C2AD',
+      'dark': '#00474B',
+      'heading': '#3E6465',
+      'input-bg': '#F3F8FB',
+      'grey-txt': '#637171',
+      'error': '#B57B6D',
+      'white': '#ffffff',
+    }
+  },
+  plugins: [],
+}
