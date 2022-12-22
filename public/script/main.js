afd229e3eb2e4c9102bf4261f02e1b13f60b8cbe
Anastasiia <anastasia.alexeenko@gmail.com>
2022-12-22 19:31:39 +0100

    📐 some algo worked on
    math formula ok but still no clue on how to optimize the code

--- public/script/main.js
+++ public/script/main.js
@@ -1,32 +1,46 @@
+// inputs got from document
 let inputBill = document.getElementById('bill');
 let inputTip = document.getElementById('tip');
 let inputPeople = document.getElementById('people');
 
+// error messages
+const errorBill = document.getElementById('error-bill');
+const errorPeople = document.getElementById('error-people');
+
+// result elements (noninteractive)
 const tipAmountPerPerson = document.getElementById('tip-amount');
 const totalPerPerson = document.getElementById('total-pp');
+
+// button reset
 const btnReset = document.getElementById('btn');
 
-// console.log(valueBill + ',' + valueTip + ',' + valuePeople);
+// tip options from document
+let tipOptions = document.querySelectorAll('.tip__option');
+console.log(tipOptions);
 
-// let valueBill, valueTip, valuePeople;
+// _____ functions for correctly/incorrectly filled inputs
+function inputBad(i,e) {
+    i.classList.add('error');
+    e.classList.remove('hidden');
+}
+
+function inputGood(i,e) {
+    i.classList.remove('error');
+    e.classList.add('hidden');
+}
+
+// array containing all inputs to apply a "foreach"
 let inputArray = [inputBill, inputTip, inputPeople];
 
-function inputBad(i) {
-    i.classList.add('error');
-}
-
-function inputGood(i) {
-    i.classList.remove('error');
-}
-
+// _____ function occuring on each input interaction
 inputArray.forEach(input => {
     input.addEventListener('keyup', () => {
         if (input.id == "bill") {
             if (inputBill.value <= 0) {
-                inputBad(inputBill);
+                inputBad(inputBill, errorBill);
                 tipAmountPerPerson.innerHTML = '0.00';
             } else {
-                inputGood(inputBill);
+                inputGood(inputBill, errorBill);
                 let valueBill = inputBill.value;
                 tipAmountPerPerson.innerHTML = (valueBill / 3).toFixed(2);
                 console.log(valueBill);
@@ -38,3 +52,16 @@ inputArray.forEach(input => {
         }
     })
 });
+
+
+function countTotal (a,b,c) {
+    let resultTotal = ((a+(a*(b/100)))/c).toFixed(2);
+    let resultTip =  (resultTotal - (a/c)).toFixed(2);
+    console.log(resultTip);
+    console.log(resultTotal);
+    tipAmountPerPerson.innerHTML = resultTip;
+    totalPerPerson.innerHTML = resultTotal;
+    btnReset.classList.add('active');
+}
+
+// countTotal(100,5,2);
